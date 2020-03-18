575524689d61b922239ca6c634be9e4ba4959775
rivmt@MacBook <devirvmt@gmail.com>
2020-03-19 08:49:48 +0900

    Add Bottom Navigation Bar

--- app/src/main/java/io/rivmt/kaliberaudiobook/MainActivity.kt
+++ app/src/main/java/io/rivmt/kaliberaudiobook/MainActivity.kt
@@ -2,11 +2,38 @@ package io.rivmt.kaliberaudiobook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Title
+        setTitle(R.string.app_name)
+        supportActionBar?.hide()
+
+        //Bottom Navigation
+        bottom_navigation.setOnNavigationItemSelectedListener (mBottomNavigationSelected)
+        bottom_navigation.selectedItemId = R.id.navigation_home
+    }
+
+    //Bottom Navigation Bar Selection
+    private val mBottomNavigationSelected = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when(menuItem.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_library -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_menu -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> return@OnNavigationItemSelectedListener false
+        }
     }
 }
